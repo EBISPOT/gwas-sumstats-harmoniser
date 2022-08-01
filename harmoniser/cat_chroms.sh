e525ea63e4a3b15338c06aced43ee996f8db97d8
jiyue1214 <52689284+jiyue1214@users.noreply.github.com>
2022-08-01 21:45:25 +0100

    Update cat_chroms.sh
    change of checking the chr$c.merged.hm (if not all chromosomes are included for running).

--- harmoniser/cat_chroms.sh
+++ harmoniser/cat_chroms.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+
 file_dir=$2
 output=$4
 
-
-head -q -n1 $file_dir/chr*.merged.hm | head -n1 > $output
-for c in {1..22}; do
+head -q -n1 $file_dir/*.merged.hm | head -n1 > $output
+for c in $(seq 1 22) X Y MT; do
+        if [ -f $file_dir/chr$c.merged.hm ]; then 
         tail -n+2  $file_dir/chr$c.merged.hm >> $output
+        fi
 done
-tail -n+2  $file_dir/chrX.merged.hm >> $output
-tail -n+2  $file_dir/chrY.merged.hm >> $output
-tail -n+2  $file_dir/chrMT.merged.hm >> $output
