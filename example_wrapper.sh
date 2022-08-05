e89ac8d2886b4012154f5cc8f26d72fa3ac7c1a5
jiyue1214 <52689284+jiyue1214@users.noreply.github.com>
2022-08-05 10:05:52 +0100

    Update example_wrapper.sh

--- example_wrapper.sh
+++ example_wrapper.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set +u;source ~/.bashrc;set -u
-set +u;source /homes/yueji/miniconda3/etc/profile.d/conda.sh;set -u
 set +u;conda init;set -u
-set +u;conda activate /homes/yueji/miniconda3/envs/conda_harmonization/;set -u 
+set +u;conda activate ./miniconda3/envs/conda_harmonization/;set -u 
 
 nextflow='path of main.nf'
-config='path of config.ymal'
+config='path of nextflow config'
 to_harm='path of Ready_to_harmonise'
 
 # Creat a folder as the launch folder (default is using date)
@@ -14,4 +12,4 @@ folder=$(date +'%d_%m_%Y')
 mkdir -p $folder
 
 cd $folder
-nextflow $nextflow -c $config --inputPath $folder --to_harm $to_harm --date +'%d_%m_%Y' -resume 
+nextflow $nextflow -c $config --inputPath $folder --to_harm_folder $to_harm -resume --execute local(or LSF)
