7f299a86fd29d2ba5bbbdfd07772b004cf853b83
jiyue1214 <52689284+jiyue1214@users.noreply.github.com>
2023-10-30 09:32:03 +0000

    allow the chr is not int

--- bin/liftover.py
+++ bin/liftover.py
@@ -42,8 +42,8 @@ def isNumber(value):
 
 
 def map_bp_to_build_via_liftover(chromosome, bp, build_map, coordinate):
-    if isNumber(bp):
-        data = build_map.convert_coordinate('chr' + str(chromosome), int(bp)-int(coordinate))
+    if isNumber(chromosome) and isNumber(bp):
+        data = build_map.convert_coordinate('chr' + str(int(chromosome)), int(bp)-int(coordinate))
         if data is not None:
             if len(data) > 0:
                 return str(data[0][1]+int(1))
